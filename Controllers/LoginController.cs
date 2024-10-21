c45a534d1e63e0e40c950aba2846386ce4eb9742
ElAllaouiMohamed <miemiet012@gmail.com>
2024-10-21 22:01:38 +0200

    Login beginnin not complete

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -19,21 +19,40 @@ public class LoginController : Controller
     [HttpPost("Login")]
     public IActionResult Login([FromBody] LoginBody loginBody)
     {
-        // TODO: Impelement login method
-        return Unauthorized("Incorrect password");
+        var LoginState = _loginService.CheckPassword(loginBody.Username!, loginBody.Password!);
+        if (LoginState == LoginStatus.Success)
+        {
+            HttpContext.Session.SetString("AdminSession", "LoggedIn");
+            HttpContext.Session.SetString("LoggedInAdmin", $"{loginBody.Username}");
+            return Ok("Admin login success");
+        }
+        else if (LoginState == LoginStatus.IncorrectPassword) return Unauthorized("Incorrect password");
+        else if (LoginState == LoginStatus.IncorrectUsername) return Unauthorized("Incorrect username");
+        return BadRequest();
     }
 
     [HttpGet("IsAdminLoggedIn")]
     public IActionResult IsAdminLoggedIn()
     {
-        // TODO: This method should return a status 200 OK when logged in, else 403, unauthorized
-        return Unauthorized("You are not logged in");
+        if (IsSessionRegisterd()) return Ok($"Logged in as {HttpContext.Session.GetString("LoggedInAdmin")}");
+        return Unauthorized("You are not logged in As Admin");
+    }
+
+    [HttpGet("IsSessionRegisterd")]
+    public bool IsSessionRegisterd()
+    {
+        return HttpContext.Session.GetString("AdminSession") == "LoggedIn";
     }
 
     [HttpGet("Logout")]
     public IActionResult Logout()
     {
+        if (IsSessionRegisterd())
+        {
+        HttpContext.Session.SetString("AdminSession", "LoggedOut");
         return Ok("Logged out");
+        }
+        return BadRequest("You are not logged in");
     }
 
 }
---
+++ Services/IAdminStorage.cs
@@ -0,0 +1,8 @@
+using StarterKit.Models;
+public interface IAdminStorage
+{
+    Task<bool> Create(Admin admin);
+    Task<bool> Delete(Guid adminId);
+    Task<Admin?> Find(Guid adminId);
+    Task<List<Admin>> FindMany(Guid[] adminIds);
+}
